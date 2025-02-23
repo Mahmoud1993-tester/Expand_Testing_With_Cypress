d233db512797c445b8f71d59117448b5007f772e
Mahmoud Saber <msaber93.egy@gmail.com>
2025-02-23 15:19:35 +0200

    Web inputs testing commit

---
+++ cypress/e2e/Web inputs page.cy.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+
+describe('first test', () => {
+  it('web inputs page', () => {
+    cy.visit('/inputs')
+    cy.wait(3000)
+    cy.get("#input-number").type("1")
+    cy.get("#input-text").type("Mahmoud2020")
+    cy.get("#input-password").type("2020")
+    cy.get('[name="input-date"]').type("1993-09-29")
+    cy.wait(3000)
+    cy.contains("Display Inputs").click()
+    cy.wait(2000)
+    cy.contains("Clear Inputs").click()
+
+  })
+})
